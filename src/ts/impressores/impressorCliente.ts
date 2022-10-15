f5f81175e6a729a38fccca06dc1feda1bb3e8ff0
mclaralvs <mclaralvs@gmail.com>
2022-10-15 17:17:49 -0300

    ☁: Finalizado registro de documentos.

---
+++ src/ts/impressores/impressorCliente.ts
@@ -0,0 +1,30 @@
+import Impressor from "../interfaces/impressor";
+import Cliente from "../modelos/cliente";
+import ImpressorDocumentos from "./impressorDocumentos";
+import ImpressorEndereco from "./impressorEndereco";
+
+export default class ImpressaorCliente implements Impressor {
+    private cliente: Cliente
+    private impressor!: Impressor
+
+    constructor(cliente: Cliente) {
+        this.cliente = cliente
+
+    }
+    imprimir(): string {
+        let impressao = `****************************\n`
+            + `| Nome: ${this.cliente.Nome}\n`
+            + `| Nome social: ${this.cliente.NomeSocial}\n`
+            + `| Data de nascimento: ${this.cliente.DataNascimento.toLocaleDateString()}\n`
+            + `| Data de cadastro: ${this.cliente.DataCadastro.toLocaleDateString()}\n`
+
+        this.impressor = new ImpressorEndereco(this.cliente.Endereco)
+        impressao = impressao + `\n${this.impressor.imprimir()}\n`
+
+        this.impressor = new ImpressorDocumentos(this.cliente.Documentos)
+        impressao = impressao + `\n${this.impressor.imprimir()}\n`
+
+        impressao = impressao + `\n****************************\n`
+        return impressao
+    }
+}
